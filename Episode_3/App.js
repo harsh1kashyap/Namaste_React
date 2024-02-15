eb5a260b8e23bc24670a41b7ac770f81a5cef839
harsh1kashyap <harshvardhanjha4@gmail.com>
2024-02-15 22:39:40 +0530

    tt

--- Episode_3/App.js
+++ Episode_3/App.js
@@ -7,7 +7,7 @@ const heading = React.createElement("h1", { id: "heading" }, "This is React Head
 //  Now Insted of creating element when can use JSX
 //jsx is not same like JS
 
-const jsxHeading = <h1 id="heading">Hello, I'm JSX Heading </h1>
+const jsxHeading = () => { (<h1 id="heading">Hello, I'm JSX Heading </h1>) }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
